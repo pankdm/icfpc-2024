221365f18894836443371305be49354143ee50a3
Oleg Godovykh <oleg.godovykh@gmail.com>
2024-06-29 19:22:11 -0700

    Parse CLI args

--- ide/cli/src/commonMain/kotlin/dev/icfpc/icfpc2024/Runner.kt
+++ ide/cli/src/commonMain/kotlin/dev/icfpc/icfpc2024/Runner.kt
@@ -1,10 +1,21 @@
 package dev.icfpc.icfpc2024
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.default
+import com.github.ajalt.clikt.parameters.arguments.help
+import com.github.ajalt.clikt.parameters.types.file
+import com.github.ajalt.clikt.parameters.types.int
 
-class Runner : CliktCommand() {
+class Runner : CliktCommand(
+    printHelpOnEmptyArgs = true,
+) {
+    private val input by argument().file(mustExist = true).help("Input file for 3d task")
+    private val a by argument().int().help("Parameter A to be set for runner").default(0)
+    private val b by argument().int().help("Parameter B to be set for runner").default(0)
+
     override fun run() {
-        println("Kotlin goes brrr")
+        println("Running ${input.canonicalPath} with A=$a and B=$b")
     }
 }
 
